8191e3fc243f917e50adbefe62baff5b0ade720b
Brad <brad@brad.tw>
2019-09-19 21:30:57 +0800

    2019091902

--- app/src/main/java/com/example/bletest4/MainActivity.java
+++ app/src/main/java/com/example/bletest4/MainActivity.java
@@ -13,13 +13,33 @@ import android.util.Log;
 import android.view.View;
 
 import com.inuker.bluetooth.library.BluetoothClient;
+import com.inuker.bluetooth.library.connect.listener.BleConnectStatusListener;
+import com.inuker.bluetooth.library.connect.options.BleConnectOptions;
+import com.inuker.bluetooth.library.connect.response.BleConnectResponse;
+import com.inuker.bluetooth.library.model.BleGattProfile;
 import com.inuker.bluetooth.library.search.SearchRequest;
 import com.inuker.bluetooth.library.search.SearchResult;
 import com.inuker.bluetooth.library.search.response.SearchResponse;
 
+import static com.inuker.bluetooth.library.Constants.STATUS_CONNECTED;
+import static com.inuker.bluetooth.library.Constants.STATUS_DISCONNECTED;
+
 public class MainActivity extends AppCompatActivity {
     private BluetoothClient mClient;
     private BluetoothDevice bleDevice;
+    private String mac;
+    private final BleConnectStatusListener mBleConnectStatusListener = new BleConnectStatusListener() {
+
+        @Override
+        public void onConnectStatusChanged(String mac, int status) {
+            Log.v("brad", "debug");
+            if (status == STATUS_CONNECTED) {
+                Log.v("brad", "connect OK");
+            } else if (status == STATUS_DISCONNECTED) {
+                Log.v("brad", "disconnect");
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,13 +82,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDeviceFounded(SearchResult device) {
                 BluetoothDevice btdevice = device.device;
-                String mac = device.getAddress();
+                //String mac = device.getAddress();
 
 
                 if (device.getName() != null && device.getName().contains("Brad")){
                     mClient.stopSearch();
                     bleDevice = btdevice;
-                    Log.v("brad", "got it");
+                    mac = device.getAddress();
+                    Log.v("brad", "got it:" + mac);
                 }
 
 
@@ -90,5 +111,25 @@ public class MainActivity extends AppCompatActivity {
 
     public void stopScanDevices(View view) {
         mClient.stopSearch();
+    }
+
+    public void connectDevices(View view) {
+        BleConnectOptions options = new BleConnectOptions.Builder()
+                .setConnectRetry(3)   // 连接如果失败重试3次
+                .setConnectTimeout(30000)   // 连接超时30s
+                .setServiceDiscoverRetry(3)  // 发现服务如果失败重试3次
+                .setServiceDiscoverTimeout(20000)  // 发现服务超时20s
+                .build();
+        Log.v("brad", "debug2:" + mac);
+        mClient.registerConnectStatusListener(mac, mBleConnectStatusListener);
+        mClient.connect(mac, new BleConnectResponse() {
+            @Override
+            public void onResponse(int code, BleGattProfile data) {
+                Log.v("brad", "code = " + code);
+            }
+        });
+    }
+    public void disconnectDevices(View view) {
+        mClient.disconnect(mac);
     }
 }
